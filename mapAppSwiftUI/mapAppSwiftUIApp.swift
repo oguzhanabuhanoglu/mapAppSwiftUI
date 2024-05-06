a83c1ccbbfec76f387effb8b164385680e28bf19
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-05-06 23:55:38 +0300

    assets added and mvvm group created

---
+++ mapAppSwiftUI/mapAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapAppSwiftUIApp.swift
+//  mapAppSwiftUI
+//
+//  Created by Oğuzhan Abuhanoğlu on 6.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct mapAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
